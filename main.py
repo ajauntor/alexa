c6b90962bd379c6bcc34b5e373eb70a3bba59df6
AJ Auntor <auntorknocks@gmail.com>
2021-05-28 13:14:41 +0600

    import all pakages

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+# import speech_recognition as sr
+
+# listener = sr.Recognizer()
+# try:
+#     with sr.Microphone() as source:
+#         print("lishing...")
+#         voice = listener.listen(source)
+#         command = listener.recognize_google(voice)
+#         print(command)
+# except:
+#     pass
 import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia 
+import pyjokes
 
 listener = sr.Recognizer()
-try:
-    with sr.Microphone() as source:
-        print("lishing...")
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        print(command)
-except:
-    pass
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
